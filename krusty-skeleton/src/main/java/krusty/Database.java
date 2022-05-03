ff44bbf05338481bd2fbe87202a8789b791b0564
crabking2007 <100423429+crabking2007@users.noreply.github.com>
2022-05-03 12:31:59 +0200

    Add files via upload

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -3,9 +3,8 @@ package krusty;
 import spark.Request;
 import spark.Response;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
+import java.sql.*;
+import java.util.*;
 
 import static krusty.Jsonizer.toJson;
 
@@ -13,33 +12,77 @@ public class Database {
 	/**
 	 * Modify it to fit your environment and then use this string when connecting to your database!
 	 */
-	private static final String jdbcString = "jdbc:mysql://localhost/krusty";
+	private static final String jdbcString = "jdbc:mysql://puccini.cs.lth.se:3306/hbg13";
 
 	// For use with MySQL or PostgreSQL
-	private static final String jdbcUsername = "<CHANGE ME>";
-	private static final String jdbcPassword = "<CHANGE ME>";
+	private static final String jdbcUsername = "hbg13";
+	private static final String jdbcPassword = "pjd666zk";
+
+	private Connection conn;
 
 	public void connect() {
-		// Connect to database here
+		try {
+			conn = DriverManager.getConnection(jdbcString, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	// TODO: Implement and change output in all methods below!
 
 	public String getCustomers(Request req, Response res) {
-		return "{}";
+		String sql = "SELECT name AS name, address FROM K_customers";
+		String title = "customers";
+		try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            return Jsonizer.toJson(rs, title);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null; 
 	}
 
 	public String getRawMaterials(Request req, Response res) {
-		return "{}";
+		String sql = "SELECT name, amount, unit FROM K_rawmaterials";
+		String title = "raw-materials";
+		try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            return Jsonizer.toJson(rs, title);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null; 
 	}
 
 	public String getCookies(Request req, Response res) {
-		return "{\"cookies\":[]}";
+		String sql = "SELECT cookieName AS name FROM K_cookies";
+		String title = "cookies";
+		try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            return Jsonizer.toJson(rs, title);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null; 
 	}
 
 	public String getRecipes(Request req, Response res) {
-		return "{}";
+		String sql = "SELECT cookieName, rawmaterials_name, amount, unit FROM K_recipes";
+		String title = "recipes";
+
+		try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            return Jsonizer.toJson(rs, title);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null; 
 	}
+
 
 	public String getPallets(Request req, Response res) {
 		return "{\"pallets\":[]}";
@@ -50,6 +93,8 @@ public class Database {
 	}
 
 	public String createPallet(Request req, Response res) {
+		//String cookie = req.queryParams("cookie");
 		return "{}";
 	}
+
 }
